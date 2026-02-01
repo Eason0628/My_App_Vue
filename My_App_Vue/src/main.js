aedde3ff7cb069f8f2f19b5c9ed1fe6450ed19bb
Eason0628 <zhaowintoo@gmail.com>
2026-02-01 22:30:57 +0800

    5.npm install ant-design-vue @ant-design/icons-vue; complete login component;

--- My_App_Vue/src/main.js
+++ My_App_Vue/src/main.js
@@ -3,6 +3,10 @@ import './style/index.scss'
 import App from './App.vue'
 import router from './js/router.js'
 import './mock/index.js'
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+
 const app = createApp(App);
 app.use(router);
+app.use(Antd);
 app.mount('#app');
--- My_App_Vue/src/page/login/index.vue
+++ My_App_Vue/src/page/login/index.vue
@@ -1,17 +1,135 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="login">
+    <div class="lgbg">
+      <div class="part">
+        <div class="title">
+          <div class="logo">
+            <img :src="logo" :width="100" alt="logo" />
+          </div>
+          <h1>{{ title }}</h1>
+        </div>
+
+        <!-- 登录表单 -->
+        <a-form :model="user" :rules="rules" :form="form" class="login-form">
+          <a-form-item name="username">
+            <a-input
+              :value="user.username"
+              placeholder="Please input your username"
+            >
+              <template #prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </a-form-item>
+
+          <a-form-item name="password">
+            <a-input-password
+              :value="user.password"
+              placeholder="Please input your password"
+            >
+              <template #prefix>
+                <LockOutlined />
+              </template>
+            </a-input-password>
+          </a-form-item>
+
+          <a-form-item>
+            <a-button
+              type="primary"
+              block
+              :loading="loading"
+              @click="handleLogin"
+            >
+              Login
+            </a-button>
+          </a-form-item>
+        </a-form>
+      </div>
+    </div>
   </div>
 </template>
 
-<script >
+<script setup>
+import { reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+import { Form, message } from "ant-design-vue";
+import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
 import { login } from "@/js/service/user.js";
-import { onMounted } from "vue";
-export default {
-  name: "Login",
-  setup () {
 
-    return {}
+const router = useRouter();
+
+const form = Form.useForm();
+const loading = ref(false);
+
+const user = reactive({
+  username: "",
+  password: "",
+});
+const rules = {
+  username: [{ required: true, message: "Please input your username", trigger: "blur" }],
+  password: [{ required: true, message: "Please input your password", trigger: "blur" }],
+};
+
+const title = "Intelegent Park";
+const logo = "/images/logo.png";
+const handleLogin = async () => {
+
+  try {
+    loading.value = true;
+    await form.validate();
+    const { data } = await login(user);
+    const { token, username, btnAuth } = data;
+
+    sessionStorage.setItem("username", username);
+    sessionStorage.setItem("btnAuth", JSON.stringify(btnAuth));
+    sessionStorage.setItem("token", token);
+
+    message.success("Login success");
+    router.replace("/");
+  } catch (err) {
+    console.error(err);
+    message.error("Login failed");
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url('/images/bg.jpg');
+
+  .lgbg {
+    width: 1200px;
+    height: 700px;
+    background-image: url('/images/lgbg.jpg');
+    .part {
+      width: 364px;
+      height: 560px;
+      padding: 140px 20px 0px 20px;
+      background-color: white;
+      margin-left: auto;
+      .title {
+        margin-bottom: 50px;
+        .logo {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        h1 {
+          text-align: center;
+          color: #027cc5;
+          margin-top: 40px;
+        }
+      }
+    }
   }
 }
-</script>
+</style>
